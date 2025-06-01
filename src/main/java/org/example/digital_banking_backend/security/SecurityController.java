42ffe0e1ab5643cf634fe55e0f8122ca3b3da0e5
Ali <ahmedalielm02@gmail.com>
2025-06-01 19:06:09 +0100

    uu

---
+++ src/main/java/org/example/digital_banking_backend/security/SecurityController.java
@@ -0,0 +1,86 @@
+package org.example.digital_banking_backend.security;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
+
+    @GetMapping("/profile")
+    public Authentication authentication(Authentication authentication) {
+        return authentication;
+    }
+
+
+    @PostMapping("/login")
+    public Map<String, String> login(String username, String password) {
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        Instant now = Instant.now();
+
+        String scope = authentication.getAuthorities().stream()
+                .map(a -> a.getAuthority())
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(now.plus(10, ChronoUnit.MINUTES))
+                .subject(username)
+                .claim("scope", scope)
+                .build();
+
+        JwtEncoderParameters jwtEncoderParameters =
+                JwtEncoderParameters.from(
+                        JwsHeader.with(MacAlgorithm.HS512).build(),
+                        jwtClaimsSet
+                );
+
+        String jwt = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+
+        return Map.of("access_token", jwt);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
